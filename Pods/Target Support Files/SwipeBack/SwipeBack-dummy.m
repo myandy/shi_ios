6f7ad704561500289e029e908380e0527bddc9c8
myandy <489321926@qq.com>
2017-04-19 22:52:27 +0800

    delete pods

--- Pods/Target Support Files/SwipeBack/SwipeBack-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SwipeBack : NSObject
-@end
-@implementation PodsDummy_SwipeBack
-@end
--- Pods/Target Support Files/iCarousel/iCarousel-umbrella.h
+++
@@ -1,9 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-#import "iCarousel.h"
-
-FOUNDATION_EXPORT double iCarouselVersionNumber;
-FOUNDATION_EXPORT const unsigned char iCarouselVersionString[];
-
